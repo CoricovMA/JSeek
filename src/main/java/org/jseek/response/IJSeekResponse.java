340084eae78acbbceb78f36cf25bafb585bfa283
CoricovacMA <coricovacMA@gmail.com>
2020-08-26 23:20:13 -0400

    fixed some abstract classes

--- src/main/java/org/jseek/response/IJSeekResponse.java
+++ src/main/java/org/jseek/response/IJSeekResponse.java
@@ -4,14 +4,18 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public abstract class IJSeekResponse {
 
-    private MessageReceivedEvent event;
+    private final MessageReceivedEvent event;
 
     IJSeekResponse(MessageReceivedEvent event){
         this.event = event;
     }
 
     public void send() {
-        event.getChannel().sendMessage("This send method has not yet been implemented").queue();
+        event.getChannel().sendMessage("This command has not yet been implemented.").queue();
+    }
+
+    protected MessageReceivedEvent getEvent(){
+        return this.event;
     }
 
 }
--- src/main/java/org/jseek/response/InfoResponse.java
+++ src/main/java/org/jseek/response/InfoResponse.java
@@ -4,8 +4,13 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class InfoResponse extends IJSeekResponse {
 
-
     public InfoResponse(MessageReceivedEvent event) {
         super(event);
     }
+
+    @Override
+    public void send() {
+        this.getEvent().getChannel().sendMessage("").queue();
+    }
+
 }
