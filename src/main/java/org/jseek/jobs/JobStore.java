45ba784dd36c00e1e21022ab786f10bb4dff91c2
coricovMA <coricovacMA@gmail.com>
2021-04-04 22:06:32 -0400

    switching to loading cache

--- src/main/java/org/jseek/jobs/JobStore.java
+++ src/main/java/org/jseek/jobs/JobStore.java
@@ -1,18 +1,50 @@
 package org.jseek.jobs;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class JobStore {
 
     private static JobStore instance;
+    private static LoadingCache<String, ArrayList<String>> userCache;
+    private static LoadingCache<String, ArrayList<String>> searchCache;
     private static Map<Parser, List<String>> store;
 
     public enum Parser{
         INDEED,
         GOOGLE;
+    }
+
+    public static void init(){
+        userCache = CacheBuilder
+                .newBuilder()
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .build(
+                        new CacheLoader<>() {
+                            @Override
+                            public ArrayList<String> load(String s) throws Exception {
+                                return null;
+                            }
+                        }
+                );
+
+        searchCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .build(
+                        new CacheLoader<>() {
+                            @Override
+                            public ArrayList<String> load(String s) throws Exception {
+                                return null;
+                            }
+                        }
+                );
     }
 
     private JobStore(){
